71906d8675b802b8133e0307aaea72728b9be247
Vincent B <vinbryant95@gmail.com>
2018-02-28 11:08:51 -0500

    Website functionality complete

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -11,10 +11,11 @@ namespace MvcMovie.Models
         public string Title { get; set; }
 
         [Display(Name = "Release Date"), DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
         [Required, StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$", ErrorMessage = "Genre may not contain numbers.")]
         public string Genre { get; set; }
 
         [Range(1, 100), DataType(DataType.Currency)]
